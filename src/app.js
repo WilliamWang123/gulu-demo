a9f6e9b168b8893813bf2ae95ae8274d12ad86c4
恋仔 <1842563969@qq.com>
2020-10-01 22:29:48 +0800

    使用 chai.spy 监听回调函数

--- src/app.js
+++ src/app.js
@@ -18,6 +18,8 @@ new Vue({
 })
 
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
 
 const expect = chai.expect
 
@@ -95,9 +97,10 @@ const expect = chai.expect
     }
   })
   vm.$mount()
-  vm.$on('click', function () {
-    console.log(1);
-  })
+  let spy = chai.spy(function () { })
+
+  vm.$on('click', spy)
   let button = vm.$el
   button.click()
+  expect(spy).to.have.been.called()
 }
